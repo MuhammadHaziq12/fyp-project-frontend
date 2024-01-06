255d3127e78aba2897c65c52f17bd81834c0b153
Muhammad Haziq <muhammadhaziq.myself@gmail.com>
2024-01-06 22:00:58 +0500

    navAndSide v1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,93 +1,111 @@
 'use client'
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import Sidebar from '@/components/sidebar/Sidebar'
+
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+    const [nav, setNav] = useState(false);
 
-  const links = [
-    {
-      id: 1,
-      link: "/",
-      label: "Home",
-    },
-    {
-      id: 2,
-      link: "about",
-      label: "About",
-    },
-    {
-      id: 3,
-      link: "portfolio",
-      label: "Portfolio",
-    },
-    {
-      id: 4,
-      link: "experience",
-      label: "Experience",
-    },
-    {
-      id: 5,
-      link: "contact",
-      label: "Contact",
-    },
-  ];
+    const links = [
+        {
+            id: 1,
+            link: "/",
+            label: "Home",
+        },
+        {
+            id: 2,
+            link: "about",
+            label: "About",
+        },
+        {
+            id: 3,
+            link: "portfolio",
+            label: "Portfolio",
+        },
+        {
+            id: 4,
+            link: "experience",
+            label: "Experience",
+        },
+        {
+            id: 5,
+            link: "contact",
+            label: "Contact",
+        },
+    ];
 
-  return (
-    <nav>
-      <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
-        <div>
-          <h1 className="text-5xl font-signature ml-2">
-            <a
-              className="link-underline link-underline-black"
-              href=""
-              target="_blank"
-              rel="noreferrer"
-            >
-              Logo
-            </a>
-          </h1>
+    return (
+        <div className='flex'>
+            <nav>
+                <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+                    <div className="p-7 flex flex-col">
+                        {/* <h1 className="text-4xl font-signature ml-20 order-2">
+                            <a
+                                className="link-underline link-underline-black"
+                                href=""
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Dashboard
+                            </a>
+                        </h1> */}
+                    </div>
+
+                    <ul className="hidden md:flex">
+                        {links.map(({ id, link, label }) => (
+                            <li
+                                key={id}
+                                className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+                            >
+                                {
+                                    <a href={link}>{label}</a>
+                                }
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div
+                        onClick={() => setNav(!nav)}
+                        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+                    >
+                        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+                    </div>
+
+                    {nav && (
+                        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+                            {links.map(({ id, link, label }) => (
+                                <li
+                                    key={id}
+                                    className="px-4 cursor-pointer capitalize py-6 text-4xl"
+                                >
+                                    {
+                                        <a href={link}>{label}</a>
+                                    }
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </nav>
         </div>
-
-        <ul className="hidden md:flex">
-          {links.map(({ id, link, label }) => (
-            <li
-              key={id}
-              className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
-            >
-              {link === '/' ? (
-                <span>{label}</span>
-              ) : (
-                <a href={link}>{label}</a>
-              )}
-            </li>
-          ))}
-        </ul>
-
-        <div
-          onClick={() => setNav(!nav)}
-          className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
-        >
-          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-        </div>
-
-        {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-            {links.map(({ id, link, label }) => (
-              <li
-                key={id}
-                className="px-4 cursor-pointer capitalize py-6 text-4xl"
-              >
-                {
-                  <a href={link}>{label}</a>
-                }
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </nav>
-  );
+    );
 };
 
 export default Navbar;
+
+// import React from 'react'
+
+// const Navbar = () => {
+//     return (
+//         <div className='flex'>
+//             <Sidebar />
+//             <div className='p-7'>
+//                 <h1 className='text-2xl font-semibold'>Home Page</h1>
+//             </div>
+//         </div>
+
+//     )
+// }
+
+// export default Navbar
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,21 +1,74 @@
 'use client'
 
-import {React, useState} from 'react'
-import { BsArrowLeftShort } from 'react-icons/bs'
+import { React, useState } from 'react'
+import { BsArrowLeftShort, BsChevronDown, BsPerson } from 'react-icons/bs'
+import { AiFillEnvironment, AiOutlineFileText, AiOutlineLogout, AiOutlineSetting, AiOutlineMail } from 'react-icons/ai'
+import { RiDashboardFill } from 'react-icons/ri'
 
 const Sidebar = () => {
 
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
+  const [submenuOpen, setSubmenuOpen] = useState(false)
+  const Menus = [
+    { title: "Dashboard" },
+    { 
+      title: "Pages",
+      icon: <AiOutlineFileText />,
+      submenu: true,
+      submenuItems: [
+        {title: "Add Product"},
+        {title: "view Product"},
+        {title: "Track Product"},
+      ],
+    },
+    { title: "Inbox", icon: <AiOutlineMail /> },
+    { title: "Profile", spacing: true, icon: <BsPerson /> },
+    { title: "Setting", icon: <AiOutlineSetting /> },
+    { title: "Logout", icon: <AiOutlineLogout /> },
+  ];
 
   return (
-    <div className='flex'>
-      <div className={`bg-purple-950 h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
+      <div className={`bg-customColor1 h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
         <BsArrowLeftShort className={`bg-cyan-500 text-3xl rounded-full absolute -right-3 top-9 border border-purple-950 cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+
+        <div className='inline-flex'>
+          <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"
+            }`} />
+          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>SagriChain</h1>
+        </div>
+
+        <ul className='pt-2'>
+            {Menus.map((menu, index) => (
+              <>
+              <li key={index} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-customColor2 rounded-md ${menu.spacing ? "mt-9" : "mt-2"} `}>
+                <span className='text-2xl block float-left'>
+                  {menu.icon ? menu.icon : <RiDashboardFill />}
+                </span>
+                <span className={`text-base font-medium flex-1 duration-200 ${!open && "hidden"}`}>
+                  {menu.title}
+                </span>
+                {menu.submenu && open && (
+                  <BsChevronDown className={`${submenuOpen &&
+                    "rotate-180" }`} onClick={() => setSubmenuOpen
+                    (!submenuOpen) } />
+                )}
+              </li>
+              {menu.submenu && submenuOpen && open && (
+                <ul>
+                  {menu.submenuItems.map((submenuItem, index) => (
+                    <li key={index} className='text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-customColor2 rounded-md'>
+                      {submenuItem.title}
+                    </li>
+                  ))}
+                </ul>
+              )}
+              </>
+            ))}
+        </ul>
+
       </div>
-      <div className='p-7'>
-        <h1 className='text-2xl font-semibold'>Home Page</h1>
-      </div>
-    </div>
+    
+    
   )
 }
 
